bfdfef52d3197e3a4171903562e681942ccd515a
Solène <45456425+SolHaine@users.noreply.github.com>
2018-12-13 12:43:04 +0100

    fonctions

--- main.js
+++ main.js
@@ -10,44 +10,45 @@ function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
 
+
+ 
 const typesList = {
 	"types": [9/*fire*/, 10/*water*/, 11/*grass*/]
 }
 
-fetch('https://pokeapi.co/api/v2/type/').then(response => response.json).then(data => {
-	types = data;
-	console.log(types)
-	
-	function selectType(typesList) {
+function selectType(typesList) {
 		const nb = getRandomInt(3);
 		selectedType = typesList.types[nb];	
 	}
-	new selectType(typesList);
+	
+selectType(typesList);
+
+console.log(selectedType)
+
+
+
+fetch('https://pokeapi.co/api/v2/type/').then(response => response.json).then(data => {
+	types = data;
+	console.log(types)
+});
+
 fetch(types.result[selectedType].url).then(response => response.json).then(data => {
 	pokemons = data;
 });
 
-});
-
-
-
-
-
-
-
-const selectPokemon = () => {
+function selectPokemon() {
 	const nb = getRandomInt(60);
-	selectedPokemon = pokemons.pokemon[nb].pokemon.name;
+	selectedPokemonName = pokemons.pokemon[nb].pokemon.name;
 	fetch(pokemons.pokemon[nb].pokemon.url).then(response =>response.json).then(data => {
 		const pokemonInfo = data;
 		selectedPokemonImg = pokemonInfo.sprites.front_default;
 	})
 }
 
+console.log(selectedPokemonName)
 
 
 const selectAndShowPokeName = type => {
 	imgPokemon1.src = selectedPokemonImg;
 	namePokemon1.innerHTML = selectedPokemonName;
-	
 }
