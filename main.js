dac8b07c6734e5dff975a21f6f8d0808b47e3965
Solène <45456425+SolHaine@users.noreply.github.com>
2018-12-13 10:54:41 +0100

    JS
    inclure l'api (pas concluant)

--- main.js
+++ main.js
@@ -1,6 +1,53 @@
-const selectAndShowPokeName = type => {
-	
+const imgPokemon1 = document.querySelector('#imgPokemon1');
+let types;
+let selectedType = 0;
+let pokemons = 0;
+let selectedPokemonName = 0;
+let selectedPokemonImg = 0;
+const namePokemon1 = document.querySelector('#name-pokemon1');
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+const typesList = {
+	"types": [9/*fire*/, 10/*water*/, 11/*grass*/]
 }
 
 fetch('https://pokeapi.co/api/v2/type/').then(response => response.json).then(data => {
-	const types = data;
+	types = data;
+	console.log(types)
+	
+	function selectType(typesList) {
+		const nb = getRandomInt(3);
+		selectedType = typesList.types[nb];	
+	}
+	new selectType(typesList);
+fetch(types.result[selectedType].url).then(response => response.json).then(data => {
+	pokemons = data;
+});
+
+});
+
+
+
+
+
+
+
+const selectPokemon = () => {
+	const nb = getRandomInt(60);
+	selectedPokemon = pokemons.pokemon[nb].pokemon.name;
+	fetch(pokemons.pokemon[nb].pokemon.url).then(response =>response.json).then(data => {
+		const pokemonInfo = data;
+		selectedPokemonImg = pokemonInfo.sprites.front_default;
+	})
+}
+
+
+
+const selectAndShowPokeName = type => {
+	imgPokemon1.src = selectedPokemonImg;
+	namePokemon1.innerHTML = selectedPokemonName;
+	
+}
