4845ad9820d65d6a7c136e118f0f3a3850d23126
Solène <45456425+SolHaine@users.noreply.github.com>
2018-12-16 15:47:15 +0100

    API + responsive
    J'ai intégré l'api et fait en sorte qu'un pokemon soit choisi aléatoirement

--- main.js
+++ main.js
@@ -1,52 +1,108 @@
 const imgPokemon1 = document.querySelector('#imgPokemon1');
-let types;
-let selectedType = 0;
-let pokemons = 0;
-let selectedPokemonName = 0;
-let selectedPokemonImg = 0;
 const namePokemon1 = document.querySelector('#name-pokemon1');
+const imgPokemon2 = document.querySelector('#imgPokemon2');
+const namePokemon2 = document.querySelector('#name-pokemon2');
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
 
-
- 
 const typesList = {
 	"types": [9/*fire*/, 10/*water*/, 11/*grass*/]
 }
 
-function selectType(typesList) {
+const selectType = typesList => {
 		const nb = getRandomInt(3);
 		selectedType = typesList.types[nb];	
 	}
 	
-selectType(typesList);
 
-console.log(selectedType)
+fetch('https://pokeapi.co/api/v2/type/').then(response => response.json()).then(data => {
+		selectType(typesList);
 
-
-
-fetch('https://pokeapi.co/api/v2/type/').then(response => response.json).then(data => {
-	types = data;
-	console.log(types)
+	console.log(selectedType)
+	console.log(data)
+	
+	console.log(selectedType)
+	
+	const selectedTypeUrl = data.results[selectedType].url
+	
+	console.log(selectedTypeUrl)
+	
+	
+	fetch(selectedTypeUrl).then(response => response.json()).then(data => {
+		const nb = getRandomInt(60);
+		const selectedPokemon = data.pokemon[nb].pokemon.name;
+		const selectedPokemonUrl = data.pokemon[nb].pokemon.url;
+		
+		console.log(nb)
+		console.log(selectedPokemon)
+		console.log(selectedPokemonUrl)
+		
+		namePokemon1.innerHTML = selectedPokemon;
+		
+		fetch(selectedPokemonUrl).then(response => response.json()).then(data => {
+			const selectedPokemonImage = data.sprites.front_default;
+			
+			console.log(selectedPokemonImage)
+			
+			imgPokemon1.src = selectedPokemonImage;
+		});
+	});
 });
 
-fetch(types.result[selectedType].url).then(response => response.json).then(data => {
-	pokemons = data;
+fetch('https://pokeapi.co/api/v2/type/').then(response => response.json()).then(data => {
+		selectType(typesList);
+
+	console.log(selectedType)
+	console.log(data)
+	
+	console.log(selectedType)
+	
+	const selectedTypeUrl = data.results[selectedType].url
+	
+	console.log(selectedTypeUrl)
+	
+	
+	fetch(selectedTypeUrl).then(response => response.json()).then(data => {
+		const nb = getRandomInt(60);
+		const selectedPokemon = data.pokemon[nb].pokemon.name;
+		const selectedPokemonUrl = data.pokemon[nb].pokemon.url;
+		
+		console.log(nb)
+		console.log(selectedPokemon)
+		console.log(selectedPokemonUrl)
+		
+		namePokemon2.innerHTML = selectedPokemon;
+		
+		fetch(selectedPokemonUrl).then(response => response.json()).then(data => {
+			const selectedPokemonImage = data.sprites.front_default;
+			
+			console.log(selectedPokemonImage)
+			
+			imgPokemon2.src = selectedPokemonImage;
+		});
+	});
 });
 
+/*
+fetch('https://pokeapi.co/api/v2/type/3').then(response => response.json()).then(data => {
+	const pokemons = data;
+	console.log(selectedType)
+	console.log(pokemons)
+});
+*/
+
+/*
 function selectPokemon() {
 	const nb = getRandomInt(60);
-	selectedPokemonName = pokemons.pokemon[nb].pokemon.name;
-	fetch(pokemons.pokemon[nb].pokemon.url).then(response =>response.json).then(data => {
+	const selectedPokemonName = pokemon[nb].pokemon.name;
+	fetch(pokemon[nb].pokemon.url).then(response =>response.json()).then(data => {
 		const pokemonInfo = data;
-		selectedPokemonImg = pokemonInfo.sprites.front_default;
+		const selectedPokemonImg = pokemonInfo.sprites.front_default;
 	})
 }
-
-console.log(selectedPokemonName)
-
+*/
 
 const selectAndShowPokeName = type => {
 	imgPokemon1.src = selectedPokemonImg;
--- main_pokegamble.js
+++ main_pokegamble.js
@@ -83,3 +83,93 @@ const returnPage1 = (button, popup, modal) => {
 button.addEventListener('click', event => changePage(event, button));
 FIGHT.addEventListener('click', event => openModule(FIGHT, modal, popup));
 retry.addEventListener('click', event => returnPage1(retry,modal,popup));
+
+
+//PARTIE AVEC L'API
+
+const imgPokemon1 = document.querySelector('#imgPokemon1');
+const namePokemon1 = document.querySelector('#name-pokemon1');
+const imgPokemon2 = document.querySelector('#imgPokemon2');
+const namePokemon2 = document.querySelector('#name-pokemon2');
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+const typesList = {
+	"types": [9/*fire*/, 10/*water*/, 11/*grass*/]
+}
+
+const selectType = typesList => {
+		const nb = getRandomInt(3);
+		selectedType = typesList.types[nb];	
+	}
+	
+
+fetch('https://pokeapi.co/api/v2/type/').then(response => response.json()).then(data => {
+	selectType(typesList);
+
+	console.log(selectedType)
+	console.log(data)
+	
+	console.log(selectedType)
+	
+	const selectedTypeUrl = data.results[selectedType].url
+	
+	console.log(selectedTypeUrl)
+	
+	
+	fetch(selectedTypeUrl).then(response => response.json()).then(data => {
+		const nb = getRandomInt(60);
+		const selectedPokemon = data.pokemon[nb].pokemon.name;
+		const selectedPokemonUrl = data.pokemon[nb].pokemon.url;
+		
+		console.log(nb)
+		console.log(selectedPokemon)
+		console.log(selectedPokemonUrl)
+		
+		namePokemon1.innerHTML = selectedPokemon;
+		
+		fetch(selectedPokemonUrl).then(response => response.json()).then(data => {
+			const selectedPokemonImage = data.sprites.front_default;
+			
+			console.log(selectedPokemonImage)
+			
+			imgPokemon1.src = selectedPokemonImage;
+		});
+	});
+});
+
+fetch('https://pokeapi.co/api/v2/type/').then(response => response.json()).then(data => {
+		selectType(typesList);
+
+	console.log(selectedType)
+	console.log(data)
+	
+	console.log(selectedType)
+	
+	const selectedTypeUrl = data.results[selectedType].url
+	
+	console.log(selectedTypeUrl)
+	
+	
+	fetch(selectedTypeUrl).then(response => response.json()).then(data => {
+		const nb = getRandomInt(60);
+		const selectedPokemon = data.pokemon[nb].pokemon.name;
+		const selectedPokemonUrl = data.pokemon[nb].pokemon.url;
+		
+		console.log(nb)
+		console.log(selectedPokemon)
+		console.log(selectedPokemonUrl)
+		
+		namePokemon2.innerHTML = selectedPokemon;
+		
+		fetch(selectedPokemonUrl).then(response => response.json()).then(data => {
+			const selectedPokemonImage = data.sprites.front_default;
+			
+			console.log(selectedPokemonImage)
+			
+			imgPokemon2.src = selectedPokemonImage;
+		});
+	});
+});
